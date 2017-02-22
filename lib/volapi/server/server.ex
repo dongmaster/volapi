d913d96b14b939dd9577f9a60ff77b154b13fc2f
dongmaster <dongmaster@users.noreply.github.com>
2017-02-22 23:58:35 +0100

    Fixed an edge case where all files are deleted from a room.

--- lib/volapi/server/server.ex
+++ lib/volapi/server/server.ex
@@ -101,7 +101,15 @@ defmodule Volapi.Server do
   def handle_call({:get_file, file_id}, _from, state) do
     files = Map.get(state, :files)
 
-    file = Enum.filter(files, fn(x) -> x.file_id == file_id end) |> hd
+    file =
+      case Enum.filter(files, fn(x) -> x.file_id == file_id end) do
+        [] ->
+          []
+        [y] ->
+          y
+        z ->
+          hd(z)
+      end
 
     {:reply, file, state}
   end
