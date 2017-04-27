a8197c7de09835d515a8294ddf163dab2575fa09
dongmaster <dongmaster@users.noreply.github.com>
2017-04-27 18:43:46 +0200

    Changed https link to https.
    Temporary fix. Needed because of invalid cert.
    Added a redirect to room requests.

--- lib/volapi/util.ex
+++ lib/volapi/util.ex
@@ -17,7 +17,7 @@ defmodule Volapi.Util do
   def get_checksum() do
     HTTPoison.start
 
-    {:ok, %{body: body}} = HTTPoison.get("https://static.#{@server}/static/js/main.js")
+    {:ok, %{body: body}} = HTTPoison.get("http://static.#{@server}/static/js/main.js")
 
     # This returns the checksum. thanks dodos for the regex
     Regex.run(~r/config\.checksum\s*=\s*"(\w+?)"/, body) |> Enum.at(1)
@@ -27,7 +27,7 @@ defmodule Volapi.Util do
     import EEx
     HTTPoison.start
 
-    {:ok, %{body: body}} = HTTPoison.get(EEx.eval_string(@volafile_room_url, [room: room]))
+    {:ok, %{body: body}} = HTTPoison.get(EEx.eval_string(@volafile_room_url, [room: room]), [], [{:follow_redirect, true}])
 
     text = Regex.replace(~r'(\w+):(?=([^"\\]*(\\.|"([^"\\]*\\.)*[^"\\]*"))*[^"]*$)', body, "\"\\1\":")
     |> String.replace("\n", "")
